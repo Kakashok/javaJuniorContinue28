c469b124356632b1187c7f74140f7df0f3832740
Victor Pomadchin <kakashoker@gmail.com>
2022-05-29 20:07:34 +0300

    Changes

--- src/main/java/controllers/StudentsController.java
+++ src/main/java/controllers/StudentsController.java
@@ -6,12 +6,35 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 @WebServlet(name = "StudentsController", urlPatterns = "/students")
 public class StudentsController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("<h1>Students page</h1>");
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/students_28_29?user=root&password=QWERTY1234");
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM student");
+
+            while (rs.next()){
+                System.out.println(rs.getInt("id"));
+                System.out.println(rs.getString("surname"));
+                System.out.println(rs.getString("name"));
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        req.getRequestDispatcher("/WEB-INF/jsp/students.jsp").forward(req, resp);
     }
 }
